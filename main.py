348668773d4b89c5f15e809e4bf7e8c89a7ce3a8
is-ux <60522734+is-ux@users.noreply.github.com>
2022-11-29 11:35:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,42 @@ def multi_sort(arr, cmp, method="None"):
 
 
 
+def cmp(a,b):
+    if a > b:
+        return -1
+    elif a == b
+        return 0
+    else
+        return 1
 
-# must be in-place sort
 def merge_sort(arr,cmp):
+    
     pass
 
+def partition(arr,cmp(arr[0],arr[len(arr)-1]))
+pvt = arr[0]
+    low = arr[0] + 1
+    high = arr[len(arr)-1]
+
+    while True:
+        while low <= high and cmp(array[high], pivot):
+            high = high - 1
+
+        while low <= high and not cmp(array[low], pivot):
+            low = low + 1
+
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+
+    array[start], array[high] = array[high], array[0]
+
+    return high
+
 # must be in-place sort
-def quick_sort(arr,cmp):
+def quick_sort(arr,cmp(arr[0],arr[len(arr)-1])):
+    p = partition(array, start, end, compare_func)
+    quick_sort(arr, arr[0], p-1, cmp(arr[0],arr[len(arr)-1]))
+    quick_sort(arr, p+1, arr[len(arr)-1], cmp(arr[0],arr[len(arr)-1]))
     pass
